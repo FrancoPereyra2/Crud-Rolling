e19557b57024dd624bd8f479bccd7828e26ec280
Franco Pereyra <francopereyra325@gmail.com>
2024-08-08 16:11:54 -0300

    agregamos modulos en el admin.js

---
+++ JS/admin.js
@@ -0,0 +1,6 @@
+import {Contacto} from "./classContacto.js";
+//aqui agrego la logica del CRUD
+
+console.log('prueba desde admin.js');
+const contacto = new Contacto('Pereyra', 'Franco', 'franco@mail.com', 123456789, 'sdfsf', 'Calle 123');
+console.log(contacto);
